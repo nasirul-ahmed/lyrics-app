6798fb7cfc027dca5606ce6e0b5450196585b984
nasirulahmed369 <76066542+nasirul-ahmed@users.noreply.github.com>
2022-02-06 20:13:52 +0530

    m

--- script.js
+++ script.js
@@ -1,35 +1,178 @@
-let btn = document.getElementById("submit");
+var content = document.getElementById("content");
+var recents = document.querySelector(".recents");
+var loading = document.querySelector(".loading");
+var seacrhText = document.getElementById("search");
+var submit = document.getElementById("submit");
+var songImage = document.querySelector("song-image");
 
-var inp = document.getElementById("search");
-var result = document.getElementById("result");
-const chartList = [];
+const chartListURL =
+  "https://shazam.p.rapidapi.com/charts/track?locale=en-US&pageSize=20&startFrom=0";
 
-const getValue = () => {
-  // var res = await fetch(
-  //   "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1&apikey=78946b90b1ff58d9b860df9ddaa3b0c2",
-  //   {
-  //     headers: {
-  //       apiKey: "78946b90b1ff58d9b860df9ddaa3b0c2",
-  //       "Content-Type": "text/plain; charset=UTF-8",
-  //     },
-  //   }
-  // );
-  // var data = await res.json();
-  // const { track_list } = data.message.body;
+const songdata = [];
 
-  result.innerHTML = "";
-  // track_list.forEach((chart) => {
+function removeDuplicates(arr) {
+  return arr.filter((item, index) => item.key === arr[index].key);
+}
 
-  for (let i = 1; i < 10; i++) {
-    var li = document.createElement("li");
+function displayData(data) {
+  loading.remove();
+  const elements = document.getElementsByClassName("recents");
+  while (elements.length > 0) {
+    elements[0].parentNode.removeChild(elements[0]);
+  }
 
-    chartList.push(li);
-    li.innerHTML = `
-      <div>
-        ${"chart.album_name " +i }
-      </div>
-    `;
-    // });
-    result.appendChild(li);
+  const ul = document.createElement("ul");
+  ul.classList.add("recents");
+
+  data.map((track) => {
+    const li = document.createElement("li");
+    li.classList.add("card");
+
+    const img = document.createElement("img");
+    img.classList.add("song-image");
+    img.src = track.images.background;
+    img.alt = "some things";
+    li.appendChild(img);
+
+    const divContainer = document.createElement("div");
+    divContainer.classList.add("container");
+    li.appendChild(divContainer);
+
+    const titleText = document.createElement("p");
+    const title = track.title;
+
+    titleText.innerHTML = `<b>${
+      title.length > 30 ? title.substring(0, 30) : title
+    }</b>`;
+    divContainer.appendChild(titleText);
+
+    const subTitile = document.createElement("p");
+    const subtitle = track.subtitle;
+    subTitile.innerText =
+      subtitle.length > 30 ? subtitle.substring(0, 15) : subtitle;
+    divContainer.appendChild(subTitile);
+
+    ul.appendChild(li);
+  });
+  content.appendChild(ul);
+}
+
+const searchSong = async (text) => {
+  if (text.length <= 0) {
+    displayData(recentSongData);
+  } else {
+    const songUrl = encodeURI(
+      `https://shazam.p.rapidapi.com/search?term=${text}&limit=5`
+    );
+
+    const res = await fetch(songUrl, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "shazam.p.rapidapi.com",
+        "x-rapidapi-key": "3eb1ec397dmsh01279653e689a5fp164f4ejsne5861f860883",
+      },
+    })
+      .then((response) => response.json())
+      .then((v) => {
+        const { hits } = v.tracks;
+        songdata.slice(0, songdata.length);
+        hits.map((hit) => {
+          songdata.push(hit.track);
+          console.log(hit);
+        });
+
+        displayData(removeDuplicates(songdata));
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 };
+
+const getRecentData = async () => {
+  try {
+    loading.innerHTML = `<h3 class="loading">Loading...</h3>`;
+    const response = await fetch(chartListURL, {
+      method: "GET",
+      headers: {
+        "x-rapidapi-host": "shazam.p.rapidapi.com",
+        "x-rapidapi-key": "3eb1ec397dmsh01279653e689a5fp164f4ejsne5861f860883",
+      },
+    });
+    const result = await response.json();
+
+    const { tracks } = result;
+
+    songdata.slice(0, songdata.length);
+
+    tracks.map((t) => songdata.push(t));
+
+    displayData(removeDuplicates(songdata));
+  } catch (error) {
+    console.log(error);
+  }
+};
+window.onload = getRecentData();
+
+const getInputData = (e) => {
+  const { value } = e.target;
+  if (content.hasChildNodes()) {
+    content.removeChild(content.childNodes[1]);
+  }
+  songdata.length = 0;
+
+  if (value == "") {
+    getRecentData();
+  } else {
+    searchSong(value);
+  }
+};
+
+seacrhText.addEventListener("input", getInputData);
+songImage.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.location.href("song_details.html");
+});
+
+// const displaySearchedData = (data) => {
+//   console.log(data);
+//   loading.remove();
+//   recents.remove();
+
+//   const ul = document.createElement("ul");
+//   ul.classList.add("recents");
+
+//   data.map((track) => {
+//     // console.log(track);
+
+//     const li = document.createElement("li");
+//     li.classList.add("card");
+
+//     const img = document.createElement("img");
+//     img.src = track.images.background;
+//     img.alt = "some things";
+
+//     li.appendChild(img);
+
+//     const divContainer = document.createElement("div");
+//     divContainer.classList.add("container");
+//     li.appendChild(divContainer);
+
+//     const titleText = document.createElement("p");
+//     const title = track.title;
+
+//     titleText.innerHTML = `<b>${
+//       title.length > 30 ? title.substring(0, 30) : title
+//     }</b>`;
+//     divContainer.appendChild(titleText);
+
+//     const subTitile = document.createElement("p");
+//     const subtitle = track.subtitle;
+//     subTitile.innerText =
+//       subtitle.length > 30 ? subtitle.substring(0, 15) : subtitle;
+//     divContainer.appendChild(subTitile);
+
+//     ul.appendChild(li);
+//   });
+//   content.appendChild(ul);
+// };
---
+++ src/data.js
@@ -0,0 +1,36 @@
+const data = [
+  {
+    layout: "5",
+    type: "MUSIC",
+    key: "104382921",
+    title: "Ginseng Strip 2002",
+    subtitle: "Yung Lean",
+
+    images: {
+      background:
+        "https://is4-ssl.mzstatic.com/image/thumb/Features125/v4/45/87/f9/4587f9b0-8bfd-91f2-f920-a665073f1607/pr_source.png/800x800cc.jpg",
+      coverart:
+        "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/e9/c4/bd/e9c4bd59-080d-329b-c763-7a7baabb9377/7071245098315_1.jpg/400x400cc.jpg",
+      coverarthq:
+        "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/e9/c4/bd/e9c4bd59-080d-329b-c763-7a7baabb9377/7071245098315_1.jpg/400x400cc.jpg",
+      joecolor: "b:1e1f24p:fffdffs:9ccb5et:d1d0d3q:83a852",
+    },
+  },
+  {
+    layout: "5",
+    type: "MUSIC",
+    key: "104382921",
+    title: "Ginseng Strip 2002",
+    subtitle: "Yung Lean",
+
+    images: {
+      background:
+        "https://is4-ssl.mzstatic.com/image/thumb/Features125/v4/45/87/f9/4587f9b0-8bfd-91f2-f920-a665073f1607/pr_source.png/800x800cc.jpg",
+      coverart:
+        "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/e9/c4/bd/e9c4bd59-080d-329b-c763-7a7baabb9377/7071245098315_1.jpg/400x400cc.jpg",
+      coverarthq:
+        "https://is3-ssl.mzstatic.com/image/thumb/Music124/v4/e9/c4/bd/e9c4bd59-080d-329b-c763-7a7baabb9377/7071245098315_1.jpg/400x400cc.jpg",
+      joecolor: "b:1e1f24p:fffdffs:9ccb5et:d1d0d3q:83a852",
+    },
+  },
+];
